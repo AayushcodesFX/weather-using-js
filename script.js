caf0d62382b8276c58e72a33d421841e084099e6
Aayush <muma5581@gmail>
2024-11-16 19:49:12 +0545

    #3

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ const searchBox = document.querySelector(".search input");
 
 const searchBtn = document.querySelector(".search button");
 
-const weatherIcon = document.querySelector(".weatherIcon");
+const weatherIcon = document.querySelector(".weather-icon");
 
 
 async function checkWeather(city){
    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-   var data = await response.json();
+
+   if(response.status == 404){
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display ="none";
+   } else {
+    var data = await response.json();
 
 
    document.querySelector(".city").innerHTML = data.name;
@@ -19,30 +24,32 @@ async function checkWeather(city){
 
    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
 
-   document.querySelector(".wind").innerHTML = data.main.wind = "Km/hr";
+   document.querySelector(".wind").innerHTML = data.main.wind = "km/hr";
 
    if(data.weather[0].main == "Clouds"){
-    weatherIcon.src = "images/clouds.png"
+    weatherIcon.src = "clouds.png"
    }
-  else if(data.weather[0].main == "Rain"){
-    weatherIcon.src = "images/rain.png"
+   else if(data.weather[0].main == "Rain"){
+    weatherIcon.src = "rain.png"
    }
-  else if(data.weather[0].main == "Clear"){
-    weatherIcon.src = "images/clear.png"
+   else if(data.weather[0].main == "Clear"){
+    weatherIcon.src = "clear.png"
    }
    else if(data.weather[0].main == "Drizzle"){
-    weatherIcon.src = "images/drizzle.png"
+    weatherIcon.src = "drizzle.png"
    }
    else if(data.weather[0].main == "Mist"){
-    weatherIcon.src = "images/mist.png"
+    weatherIcon.src = "mist.png"
    }
    else if(data.weather[0].main == "Snow"){
-    weatherIcon.src = "images/snow.png"
-   }
+    weatherIcon.src = "snow.png"
+   }}
 
+   document.querySelector(".weather").style.display = "block";
+   document.querySelector(".error").style.display = "none";
 }
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
-})
+});
 
